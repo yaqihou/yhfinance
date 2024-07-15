b3195dd9292d6b3f76b8c1f3a17c520bd9fc7c1f
Yaqi Hou <yaqi.hou@yahoo.com>
2024-07-14 21:50:40 -0400

    Refactor MyLogger to be used as Class in analogue to logging
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

--- databackup/logger.py
+++ databackup/logger.py
@@ -2,34 +2,43 @@ import sys
 import logging
 
 
-class MyLoggerSetup:
+class MyLogger:
 
     default_formatter = logging.Formatter(
-        fmt='%(asctime)s |  %(name)s::%(levelname)s | %(message)s',
+        fmt='%(asctime)s |  %(name)20s::%(levelname)6s | %(message)s',
         datefmt='%m/%d/%Y %I:%M:%S %p')
 
-    def __init__(self):
+    __PROJECT_NAME__: str | None = 'finback'
+    __LOG_FILE_NAME__: str = 'finance-data-backup.log'
+    # root logger 
+    logger = logging.getLogger(__PROJECT_NAME__)
 
-        self.logger = logging.getLogger("yfinance-backup")
-        _ = [self.logger.removeHandler(hdlr) for hdlr in self.logger.handlers]
+    @classmethod
+    def getLogger(cls, name: str | None = None):
+        if name is None or cls.__PROJECT_NAME__ is None:
+            return logging.getLogger(name)
+        else:
+            return logging.getLogger('.'.join([cls.__PROJECT_NAME__, name]))
+    
+    @classmethod
+    def setup(cls):
+        _ = [cls.logger.removeHandler(hdlr) for hdlr in cls.logger.handlers]
+        cls.logger.addHandler(cls.get_default_stdout_hdlr())
+        cls.logger.addHandler(cls.get_default_file_hdlr())
+        cls.logger.setLevel(logging.DEBUG)
 
-    def setup(self):
-        self.logger.addHandler(self.hdlr_stdout)
-        self.logger.addHandler(self.hdlr_file)
-        self.logger.setLevel(logging.DEBUG)
-
-    @property
-    def hdlr_stdout(self):
+    @classmethod
+    def get_default_stdout_hdlr(cls, level=logging.DEBUG):
         hdlr = logging.StreamHandler(sys.stdout)
-        hdlr.setFormatter(self.default_formatter)
-        hdlr.setLevel(logging.DEBUG)
+        hdlr.setFormatter(cls.default_formatter)
+        hdlr.setLevel(level)
 
         return hdlr
         
-    @property
-    def hdlr_file(self):
-        hdlr = logging.FileHandler('finance-data-backup.log')
-        hdlr.setFormatter(self.default_formatter)
-        hdlr.setLevel(logging.INFO)
+    @classmethod
+    def get_default_file_hdlr(cls, level=logging.INFO):
+        hdlr = logging.FileHandler(cls.__LOG_FILE_NAME__)
+        hdlr.setFormatter(cls.default_formatter)
+        hdlr.setLevel(level)
         return hdlr
 
