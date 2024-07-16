a46689cf826d4cf0a1c2d8184321614b45f972a2
Yaqi Hou <yaqi.hou@yahoo.com>
2024-07-16 00:04:52 -0400

    Update DB config to prod
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

--- src/yhfinance/db_utils/db_config.py
+++ src/yhfinance/db_utils/db_config.py
@@ -3,6 +3,5 @@ from pathlib import Path
 
 class DBConfig:
 
-    # DB_NAME: str = (Path.home() / 'Dropbox' / '66-DBs' / 'FinDB.db').absolute().as_posix()
-    DB_NAME= ':memory:'
+    DB_NAME: str = (Path.home() / 'Dropbox' / '66-DBs' / 'FinDB.db').absolute().as_posix()
 
