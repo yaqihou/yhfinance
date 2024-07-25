80977ed3ec68713addf861bee03f0ce1c69eb6cb
Yaqi Hou <yaqi.hou@yahoo.com>
2024-07-25 01:12:04 -0400

    Add a slicer to easily mask dataframe by time
    This is useful escepicially when dealing with indicators, for which the
    first n data points are usually unavailable.
    
    In calculation, we would prefer to use as much as possible, but then in
    plotting or other functions, we would like to just get a slice of the data
    
    Signed-off-by: Yaqi Hou <yaqi.hou@yahoo.com>

---
+++ src/yhfinance/analytics/dataslicer.py
@@ -0,0 +1,135 @@
+
+from functools import wraps
+from typing import Literal, Union, Optional, get_args
+
+import datetime as dt
+
+import numpy as np
+import pandas as pd
+
+from ._base import _OHLCBase
+from .const import Col, ColIntra, ColName
+from . import utils
+
+
+class DataSlicer(_OHLCBase):
+    """Collection of analytics for fixed time window, i.e. given year / month / week,
+    suitable for seasonality analysis
+    """
+
+    _T_CAL_INFO_COLS = Literal['Year', 'Month', 'Qtr', 'WeekNum', 'WeekDay']
+    _calendar_info_cols = get_args(_T_CAL_INFO_COLS)
+
+    def __init__(
+            self,
+            df: pd.DataFrame, tick_col: str = Col.Date.name,
+            year    : Optional[int] = None,
+            month   : Optional[int] = None,
+            qtr     : Optional[int] = None,
+            weekday : Optional[int] = None,
+            weeknum : Optional[int] = None,
+    ):
+        super().__init__(df, tick_col)
+        self._parse_calendar_info()
+
+        self._preset_year     = year
+        self._preset_month    = month
+        self._preset_qtr      = qtr
+        self._preset_weekday  = weekday
+        self._preset_weeknum  = weeknum
+
+    def __call__(self, obj: _OHLCBase | pd.DataFrame) -> Optional[pd.DataFrame]:
+
+        if isinstance(obj, pd.DataFrame):
+            return self.get_slice(
+                df      = obj                  ,
+                year    = self._preset_year    ,
+                month   = self._preset_month   ,
+                qtr     = self._preset_qtr     ,
+                weekday = self._preset_weekday ,
+                weeknum = self._preset_weeknum ,
+            )
+        else:
+            if not isinstance(obj, _OHLCBase):
+                print('Slice is supposed to be applied to a OHLC instance but given'
+                      f' instance is {obj.__class__.__name__}  may work at long as obj'
+                      ' has an attribute _df as a PandasFrame following the same step')
+            obj._df_raw = obj._df.copy()
+            obj._df = self.get_slice(
+                df      = obj._df_raw          ,
+                year    = self._preset_year    ,
+                month   = self._preset_month   ,
+                qtr     = self._preset_qtr     ,
+                weekday = self._preset_weekday ,
+                weeknum = self._preset_weeknum ,
+            )
+
+
+    def _parse_calendar_info(self):
+
+        _df = self._df[[self.tick_col]].copy()
+        # .dt can only be used with datetime but we may have dt.date type
+        _df['Year'] = _df[self.tick_col].apply(lambda x: x.date().year)
+        _df['Month'] = _df[self.tick_col].apply(lambda x: x.date().month)
+        _df['Qtr'] = _df[self.tick_col].apply(lambda x: 1 + (x.date().month - 1) // 3)
+        # For week num 53, merge it to 52
+        _df['WeekNum'] = _df[self.tick_col].apply(lambda x: max(x.date().isocalendar().week, 52))
+        _df['WeekDay'] = _df[self.tick_col].apply(lambda x: x.date().weekday())
+
+        # TODO - if for intraday, we could add intraday slice
+
+        for col in self._calendar_info_cols:
+            _df[col] = _df[col].astype(int)
+
+        self._df_calendar = _df.copy()
+
+    def get_slice(
+            self,
+            df: Optional[pd.DataFrame] = None, 
+            year: Optional[int] = None,
+            month: Optional[int] = None,
+            qtr: Optional[int] = None,
+            weekday: Optional[int] = None,
+            weeknum: Optional[int] = None,
+            copy: bool = True,
+            # ignore_calendar_info: bool = True
+    ):
+        """Return (a copy of) the slice from df falling in the given time window 
+        """
+        mask = self._df_calendar['Year'] > 0
+
+        for col, _in in [
+                ('Year', year),
+                ('Month', month),
+                ('Qtr', qtr),
+                ('WeekNum', weeknum),
+                ('WeekDay', weekday)
+        ]:
+            if _in is not None:
+                mask = mask & (self._df_calendar[col] == _in)
+
+        # cols = self._df.columns
+        # if ignore_calendar_info:
+        #     cols = list(filter(lambda x: x not in set(self.calendar_info_cols), cols))
+
+        selected_index = self._df_calendar[mask].index
+
+        df_target = df if df is not None else self._df
+        ret = df_target.loc[selected_index, :].reset_index(drop=True)
+        return ret.copy() if copy else ret
+
+    @property
+    def calendar_info_cols(self):
+        return self._calendar_info_cols
+
+    @property
+    def year_min(self):
+        return self._df['Year'].min()
+
+    @property
+    def year_max(self):
+        return self._df['Year'].max()
+
+    @property
+    def years(self) -> list[int]:
+        return sorted(self._df['Year'].unique().tolist())
